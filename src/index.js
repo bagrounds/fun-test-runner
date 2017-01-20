e6563e4361df9be6e6785257fdc1f04d06da61c1
bagrounds <bagrounds@gmail.com>
2017-01-19 19:01:39 -0800

    [MINOR][CHANGE] implementation for latest fun-test version

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@
   'use strict'
 
   /* imports */
-  var async = require('async')
   var testAnythingProtocol = require('test-anything-protocol')
   var stringify = require('stringify-anything')
 
@@ -25,36 +24,25 @@
    * @param {Object} options all function parameters
    * @param {Function} callback handle results
    */
-  function funTestRunner (options, callback) {
+  function funTestRunner (options) {
     var subject = options.subject
     var tests = options.tests
 
     tap.plan(tests.length)
 
-    var series = tests.map(function (test, index) {
-      return function (cb) {
-        test(subject, function reporter (error, options) {
-          tap.test({
-            ok: !error,
-            description: '- ' + toString(options),
-            number: index + 1
-          })
-
-          if (error) {
-            tap.diagnostic(error.message)
-          }
-
-          cb()
-        })
-      }
+    tests.map(function (test) {
+      return test(subject)
     })
+    .forEach(function (results, index) {
+      tap.test({
+        ok: !results.error,
+        description: '- ' + toString(results.options),
+        number: index + 1
+      })
 
-    async.series(series, function (error) {
-      if (error) {
-        tap.diagnostic('Error: ' + error.message)
+      if (results.error) {
+        tap.diagnostic(stringify(results.error))
       }
-
-      callback(error)
     })
   }
 
